205a10ff17c1e992052fff9c84608512995fff53
Himavaan Chandra <himavaanchandra@gmail.com>
2019-09-07 00:09:17 +1000

    python vision added

---
+++ visionPython/qrdetection.py
@@ -0,0 +1,59 @@
+import cv2
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+import sys
+import time
+
+
+cap = cv2.VideoCapture(0)
+hasFrame,frame = cap.read()
+
+# Display barcode and QR code location
+def display(im, decodedObjects):
+
+  # Loop over all decoded objects
+  for decodedObject in decodedObjects:
+    points = decodedObject.polygon
+
+    # If the points do not form a quad, find convex hull
+    if len(points) > 4 :
+      hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+      hull = list(map(tuple, np.squeeze(hull)))
+    else :
+      hull = points;
+
+    # Number of points in the convex hull
+    n = len(hull)
+
+    # Draw the convext hull
+    for j in range(0,n):
+      cv2.line(im, hull[j], hull[ (j+1) % n], (255,0,0), 3)
+
+  # Display results
+  # cv2.imshow("Results", im);
+
+# Create a qrCodeDetector Object
+qrDecoder = cv2.QRCodeDetector()
+
+# Detect and decode the qrcode
+#t = time.time()
+while(1):
+    hasFrame, inputImage = cap.read()
+    if not hasFrame:
+        break
+    decodedObjects = pyzbar.decode(inputImage)
+    if len(decodedObjects):
+        zbarData = decodedObjects[0].data
+    if zbarData:
+        cv2.putText(inputImage, "ZBAR : {}".format(zbarData), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    else:
+        cv2.putText(inputImage, "ZBAR : QR Code NOT Detected", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+    display(inputImage, decodedObjects)
+    cv2.imshow("Result",inputImage)
+    #vid_writer.write(inputImage)
+    k = cv2.waitKey(20)
+    if k == 27:
+        break
+cv2.destroyAllWindows()
+qrDecoder.release()
