7addccaaefc353d2e34b15563e72f666f2923dd4
Himavaan Chandra <himavaanchandra@gmail.com>
2019-09-09 22:29:18 +1000

    working print data

--- visionPython/qrdetection.py
+++ visionPython/qrdetection.py
@@ -1,6 +1,7 @@
 import cv2
 import pyzbar.pyzbar as pyzbar
 import numpy as np
+import roslibpy as ros
 #import sys
 #import time
 
@@ -15,23 +16,22 @@ def display(im, decodedObjects):
   for decodedObject in decodedObjects:
     points = decodedObject.polygon
 
+    if zbarData == b'1':   #points needs to be ROS published  , also store position in a variable and calculate angle using pythagoras maybe
+          print("Thomas")
+          print(points)
+    if zbarData == b'2':
+          print("Lightning") #points needs to be ROS published  , also store position in a variable and calculate angle using pythagoras maybe
+          print(points)
     # If the points do not form a quad, find convex hull
+
     if len(points) > 4 :
       hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
       hull = list(map(tuple, np.squeeze(hull)))
     else :
       hull = points
-    #print(points)   #points needs to be ROS published
-    # print(np.array(1))
+    #print(points)   
+
     # Number of points in the convex hull
-
-    if zbarData == b'1':   #maybe b'1'
-          print("Thomas")
-          print(points)
-    if zbarData == b'2':
-          print("Lightning")
-          print(points)
-
     n = len(hull)
 
     # Draw the convext hull
@@ -52,10 +52,15 @@ while(1):   #here put the 2 different qr options
         zbarData = decodedObjects[0].data
     else:
         zbarData=''
-    if zbarData:
-        cv2.putText(inputImage, "ZBAR : {}".format(zbarData), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            
+    if zbarData == b'1':
+        cv2.putText(inputImage, "Thomas: Found", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
     else:
-        cv2.putText(inputImage, "ZBAR : QR Code NOT Detected", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(inputImage, "Thomas: Not Found", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    if zbarData == b'2':
+        cv2.putText(inputImage, "Lightning: Found", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    else:
+        cv2.putText(inputImage, "Lightning: Not Found", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
     display(inputImage, decodedObjects)
     cv2.imshow("Result",inputImage)
