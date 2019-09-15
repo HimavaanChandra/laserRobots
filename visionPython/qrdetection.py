e55ed867fd4169fe76e4e0146acd25499a86c571
Himavaan Chandra <himavaanchandra@gmail.com>
2019-09-15 22:42:44 +1000

    updated changes

--- visionPython/qrdetection.py
+++ visionPython/qrdetection.py
@@ -1,71 +1,89 @@
-import cv2
+from __future__ import print_function
+
 import pyzbar.pyzbar as pyzbar
 import numpy as np
-import roslibpy as ros
-#import sys
-#import time
+import cv2
+import time
 
-
+# get the webcam:  
 cap = cv2.VideoCapture(0)
-hasFrame = cap.read() #hasFrame,frame = cap.read()
 
-# Display barcode and QR code location
-def display(im, decodedObjects):
+cap.set(3,640)
+cap.set(4,480)
+#160.0 x 120.0
+#176.0 x 144.0
+#320.0 x 240.0
+#352.0 x 288.0
+#640.0 x 480.0
+#1024.0 x 768.0
+#1280.0 x 1024.0
+time.sleep(2)
 
-  # Loop over all decoded objects
-  for decodedObject in decodedObjects:
-    points = decodedObject.polygon
+def decode(im) : 
+    # Find barcodes and QR codes
+    decodedObjects = pyzbar.decode(im)
+    # Print results
+    for obj in decodedObjects: #maybe adjust this for printing
+        # print('Type : ', obj.type)
+        # print('Data : ', obj.data,'\n')
+        print('')     
+    return decodedObjects
 
-    if zbarData == b'1':   #points needs to be ROS published  , also store position in a variable and calculate angle using pythagoras maybe
-          print("Thomas")
-          print(points)
-    if zbarData == b'2':
-          print("Lightning") #points needs to be ROS published  , also store position in a variable and calculate angle using pythagoras maybe
-          print(points)
-    # If the points do not form a quad, find convex hull
 
-    if len(points) > 4 :
-      hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
-      hull = list(map(tuple, np.squeeze(hull)))
-    else :
-      hull = points
-    #print(points)   
+font = cv2.FONT_HERSHEY_SIMPLEX
 
-    # Number of points in the convex hull
-    n = len(hull)
+while(cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+         
+    decodedObjects = decode(im)
 
-    # Draw the convext hull
-    for j in range(0,n):
-      cv2.line(im, hull[j], hull[ (j+1) % n], (255,0,0), 3)
+    for decodedObject in decodedObjects: 
+        points = decodedObject.polygon
+     
+        # If the points do not form a quad, find convex hull
+        if len(points) > 4 : 
+          hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+          hull = list(map(tuple, np.squeeze(hull)))
+        else : 
+          hull = points;
+         
+        # Number of points in the convex hull
+        n = len(hull)     
+        # Draw the convext hull
+        for j in range(0,n):
+          cv2.line(frame, hull[j], hull[ (j+1) % n], (255,0,0), 3)
 
-  # Display results
-      cv2.imshow("Results", im) #can remove
+        x = decodedObject.rect.left
+        y = decodedObject.rect.top
 
-# Detect and decode the qrcode
-#t = time.time()
-while(1):   #here put the 2 different qr options
-    hasFrame, inputImage = cap.read()
-    if not hasFrame:
+        #print(x, y)
+        qrData = decodedObject.data
+
+        if qrData == b'1':
+              print("Thomas: ",x,y)
+              # print(x,y)
+              # print("Thomas")
+              # print(x,y)
+        if qrData == b'2':
+              print("Lightning: ",x,y)
+              # print(x,y)
+
+        # print('Type : ', decodedObject.type)
+        # print('Data : ', decodedObject.data,'\n')
+
+        barCode = str(decodedObject.data)
+        cv2.putText(frame, barCode, (x, y), font, 1, (0,255,255), 2, cv2.LINE_AA)
+               
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    key = cv2.waitKey(1)
+    if key == 27:
         break
-    decodedObjects = pyzbar.decode(inputImage)
-    if len(decodedObjects):
-        zbarData = decodedObjects[0].data
-    else:
-        zbarData=''     
+   
 
-    if zbarData == b'1':
-        cv2.putText(inputImage, "Thomas: Found", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    else:
-        cv2.putText(inputImage, "Thomas: Not Found", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-    if zbarData == b'2':
-        cv2.putText(inputImage, "Lightning: Found", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    else:
-        cv2.putText(inputImage, "Lightning: Not Found", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-    display(inputImage, decodedObjects)
-    cv2.imshow("Result",inputImage)
-
-    k = cv2.waitKey(20)
-    if k == 27:
-        break
-cv2.destroyAllWindows()
+# When everything done, release the capture
 cap.release()
+cv2.destroyAllWindows()
