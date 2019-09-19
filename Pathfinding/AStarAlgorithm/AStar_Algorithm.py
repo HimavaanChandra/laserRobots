f8719f2a9df05be8424bd39f393645bd5f0738c7
Thomas Harrison <thomasluke98@outlook.com>
2019-09-19 21:58:10 +1000

    Direction readout added and CSV working

--- Pathfinding/AStarAlgorithm/AStar_Algorithm.py
+++ Pathfinding/AStarAlgorithm/AStar_Algorithm.py
@@ -31,7 +31,7 @@ class Node(): #Setting up "class" "Node"
         return self.position == other.position
 
 class AStar(): #Setting up "class" "AStar"
-    size = 1; #Sets the size of the buffer on either side of the robot
+    size = 6 #Sets the size of the buffer around the robot. MAKE SURE THAT ROBOT BUFFER IS NOT TOUCHING ANY WALLS IN THE STARTING POSITION
     # Initialisation function that is called when the class is called
     def __init__(self, maze, start, end): 
         self.maze = maze
@@ -46,6 +46,54 @@ class AStar(): #Setting up "class" "AStar"
         
     def calculatePath(self): #Can be called using "path=aStar.calculatePath()" to run the "astar" function based on the predecided "maze, start and end" conditions
         return astar(self.maze, self.size, self.start, self.end)
+
+# unit_vector = None # Variable must exist in global namespace first
+
+# def Bearing(bearing_mode): #COMPASS MODE = 0, DEGREES MODE = 1
+
+#     global unit_vector #this creates a local variable that is linked to the global variable
+    
+#     if bearing_mode == 0:
+#         if  unit_vector == (0,-1): 
+#             unit_vector='N'
+#         elif unit_vector == (1,-1): 
+#             unit_vector='NE'
+#         elif unit_vector == (1,0): 
+#             unit_vector='E'
+#         elif unit_vector == (1,1): 
+#             unit_vector='SE'    
+#         elif unit_vector == (0,1):
+#             unit_vector='S'
+#         elif unit_vector == (-1,1):
+#             unit_vector='SW'
+#         elif unit_vector == (-1,0):
+#             unit_vector='W'
+#         elif unit_vector == (-1,-1): 
+#             unit_vector='NW'
+#         elif unit_vector == (0,0): 
+#             unit_vector='NONE'
+
+#     elif bearing_mode == 1:
+#         if unit_vector == (0,-1): 
+#             unit_vector='0'
+#         elif unit_vector == (1,-1): 
+#             unit_vector='45'
+#         elif unit_vector == (1,0): 
+#             unit_vector='90'
+#         elif unit_vector == (1,1): 
+#             unit_vector='135'    
+#         elif unit_vector == (0,1):
+#             unit_vector='180'
+#         elif unit_vector == (-1,1):
+#             unit_vector='225'
+#         elif unit_vector == (-1,0):
+#             unit_vector='270'
+#         elif unit_vector == (-1,-1): 
+#             unit_vector='315'
+#         elif unit_vector == (0,0): 
+#             unit_vector='NONE'
+
+    
 
 def astar(maze, size, start, end):
     """Returns a list of tuples as a path from the given start to the given end in the given maze"""
@@ -91,13 +139,36 @@ def astar(maze, size, start, end):
                 current = current.parent #"current.parent" links to the object "new_node = Node(current_node, node_position)" further down in code
             
             directions = []
-            for item, i in path:
-                if iteration = 0:
+            for i in range(0, len(path)): # Converts each co-ordinate in the path into a direction/bearing
+                if i == 0:
                     continue
-                unit_vector = (path[i][0] - path[i+1][0], path[i][1] - path[i+1][1])
+                unit_vector = (path[i-1][0] - path[i][0], path[i-1][1] - path[i][1]) #path[i][0] == x, path[i][1] == y - Minuses the previous coordinate from each coordinate to get a unit vector
+                
+                # Bearing(0)
+
+                if  unit_vector == (0,-1): 
+                    unit_vector='N'
+                elif unit_vector == (1,-1): 
+                    unit_vector='NE'
+                elif unit_vector == (1,0): 
+                    unit_vector='E'
+                elif unit_vector == (1,1): 
+                    unit_vector='SE'    
+                elif unit_vector == (0,1):
+                    unit_vector='S'
+                elif unit_vector == (-1,1):
+                    unit_vector='SW'
+                elif unit_vector == (-1,0):
+                    unit_vector='W'
+                elif unit_vector == (-1,-1): 
+                    unit_vector='NW'
+                elif unit_vector == (0,0): 
+                    unit_vector='NONE'
+                
                 directions.append(unit_vector)
 
-            return path[::-1]  # Return reversed path
+            # return path[::-1]  # Return reversed path
+            return directions[::-1]  # Return reversed path
 
         # Generate children nodes from around the parent
         children = []
@@ -112,10 +183,10 @@ def astar(maze, size, start, end):
                 continue 
 
             # Make sure within range of maze 
-            if node_position[1] > (len(maze) - 1) or node_position[1] < 0 or node_position[0] > (len(maze[len(maze)-1]) - 1) or node_position[0] < 0:
-                continue
+            # if node_position[1] > (len(maze) - 1) or node_position[1] < 0 or node_position[0] > (len(maze[len(maze)-1]) - 1) or node_position[0] < 0:
+            #     continue
 
-            # # Make sure walkable terrain (Not in a wall)
+            # Make sure walkable terrain (Not in a wall)
             # if maze[node_position[1]][node_position[0]] != 0:
             #     continue
 
@@ -188,10 +259,10 @@ def astar(maze, size, start, end):
 
 def main(): 
 
-    cell_map = np.loadtxt("bob.csv", delimiter=",")
+    cell_map = np.loadtxt("filename.csv", delimiter=",")
     cell_array = np.array(cell_map).tolist()
    
-    aStar = AStar(cell_array, (10, 10), (15, 15)) # maze, start, end - Object "aStar" sets start and coordinates for robot
+    aStar = AStar(cell_array, (10, 10), (70, 70)) # maze, start, end - Object "aStar" sets start and coordinates for robot
     path=aStar.calculatePath() 
     print(path) #Prints coordinates of path to terminal
     # aStar.setStart((40, 40)) #Sets start positon (Should be set to equal previous path end position)
@@ -208,7 +279,7 @@ if __name__ == '__main__': #So that when/if this file is created as a header fil
 
 
 
-    # maze = np.genfromtxt('bob.csv', delimiter=',')
+    # maze = np.genfromtxt('filename.csv', delimiter=',')
     
     # MULTI DIMENSIONAL (2D) 41x41 ARRAY (INCLUDING 0) (ARRAYS WITHIN AN ARRAY)
     # x-axis 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40
