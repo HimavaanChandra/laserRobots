a24f1b7c3160f959a69357441db991719d01978b
Pranavsinghal1 <Pranav.Singhal@student.uts.edu.au>
2019-10-08 17:44:32 +1100

    Serial Comm with ROS

--- serial/Serial_com_pi.py
+++ serial/Serial_com_pi.py
@@ -1,7 +1,22 @@
 import serial
 import time
-port="/dev/ttyACM0"
 
+#ROS
+import rospkg
+import rospy
+from serial_ros.msg import serial_comms
+
+pub = rospy.Publisher('robot_positions', serial_comms, queue_size=10)
+rospy.init_node('robot_positions', anonymous=True)
+rate = rospy.Rate(10)  #10hz
+
+def callback(data):
+    rospy.loginfo("%s is age: %d" % (data.name, data.age))
+def listener():
+    rospy.init_node
+
+#Serial
+port="/dev/ttyACM0"
 s1=serial.Serial(port,9600)
 s1.flushInput()
 
@@ -58,12 +73,26 @@ while not done:
     value = my_string[index+1:len(my_string)]
     
     if command == "Angle":
-        print(command)
-        print(value)
+        #print(command)
+        #print(value)
+        
     if command == "Health":
-        print(command)
-        print(value)
+        #print(command)
+        #print(value)
+        cHealth=value
     if command == "Shoot":
-        print(value)
+        #print(value)
+        cHit=value
     if command == "Hit":
-        print(value)
+        #print(value)
+        cShoot=value
+        
+    while not rospy.is_shutdown():
+      msg = serial_comms()
+      msg.Health = cHealth
+      msg.Hit = cHit
+      msg.Shoot = cShoot
+      rospy.loginfo(msg)
+      pub.publish(msg)
+      rate.sleep()             
+
